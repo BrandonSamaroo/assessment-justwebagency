9b8030fcef38b0593741a1ec5b14a5bf577ab54d
Brandon Samaroo <samaroo.bran@gmail.com>
2022-08-11 20:05:27 -0400

    just need to sort

--- filter.py
+++ filter.py
@@ -1,9 +1,20 @@
 import json
+import sys
 
-data = open('data.json')
-
-movies = json.load(data)
-
-print(movies[0])
-
-data.close()
+def printActors():
+    data = open('data.json')
+    movies = json.load(data)
+    actors = {}
+    for movie in movies:
+        stars = movie['stars'].split(", ")
+        for star in stars:
+            if star not in actors:
+                actors[star] = {'Movies': 1, 'Rating': float(movie['rating'])}
+            else:
+                actors[star]['Movies'] += 1
+                actors[star]['Rating'] = actors[star]['Rating'] + float(movie['rating'])
+    for actor in list(actors.keys()):
+        if actors[actor]["Movies"] <= 1:
+            del actors[actor]
+    print(actors)
+    data.close()
